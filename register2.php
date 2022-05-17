feaa6b0469cc34906d6fab649d3847b6cba73f9a
HasnaAulia <81562096+HasnaAulia@users.noreply.github.com>
2022-05-17 21:27:35 +0700

    Update register2.php

--- register2.php
+++ register2.php
@@ -3,5 +3,5 @@
     session_start();
     
     mysqli_query($mysqli, "UPDATE user SET nama_user = '$_POST[nama]', tanggal_lahir = '$_POST[ttl]', umur = '$_POST[umur]', jenis_kelamin = '$_POST[JK]', alamat = '$_POST[alamat]', tipe_kendaraan = '$_POST[tipe_kendaraan]' WHERE email = '$_SESSION[email]'");
-    header('location : verifikasi.html');
+    header('location:verifikasi.html');
 ?>
