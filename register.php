5abef52b98829823285e3490b1ca1dad0458e807
HasnaAulia <81562096+HasnaAulia@users.noreply.github.com>
2022-05-17 21:27:08 +0700

    Update register.php

--- register.php
+++ register.php
@@ -5,15 +5,17 @@
     $password = $_POST['password'];
     $no_hp = $_POST['no_hp'];
 
-    $cek_akun = mysqli_num_rows(mysqli_query($mysqli, "SELECT * FROM user WHERE email = '$email'"));
-    if($cek_akun != null){
+    $sql = "SELECT email FROM user WHERE email = '$email'";
+    $cek_email = mysqli_query($dbConnection , $sql);
+    $cek = mysqli_fetch_array($cek_email, MYSQLI_NUM);
+    if($cek > 0){
         echo "Email sudah digunakan";
-        header("location: register.html");
+        header("location:register.html");
     }
     else{
         mysqli_query($mysqli, "INSERT INTO user ('email', 'no_hp', 'password') VALUES ('$email','$no_hp','$password')");
         session_start();
         $_SESSION["email"] = $email;
-        header('location : register2.html');
+        header("location:register2.html");
     }
 ?>
