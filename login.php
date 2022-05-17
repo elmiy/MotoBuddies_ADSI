6d77538900d9b759a6d1aaa52b2ff717bc693988
HasnaAulia <81562096+HasnaAulia@users.noreply.github.com>
2022-05-17 16:24:18 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+    $mysqli = new mysqli('localhost', 'root', '', 'adsi');
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $rs = $mysqli->query("CALL SP_Login ('" . $email . "','" . $password . "')");
+    $row = $rs->fetch_object();
+    if ($row) {
+        header("location: home.html");
+    } else {
+        echo "Data tidak terdaftar";
+        header('Location: login.html');
+    }
+?>
